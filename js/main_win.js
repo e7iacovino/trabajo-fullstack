64f43b155c019c04c9c2538fd7cfce2161996c5a
Emiliano Iacovino <eiacovino@escuelasproa.edu.ar>
2023-06-21 13:03:02 -0300

    21/06 3.0v

---
+++ js/main_win.js
@@ -0,0 +1,33 @@
+function auth_loc()
+{
+	var postcode, ciudad;
+      var script = document.createElement('script');
+      script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+      document.getElementsByTagName('head')[0].appendChild(script);
+      script.onload=function()
+      {
+        if (navigator.geolocation){ 
+
+          navigator.geolocation.getCurrentPosition(function(position)
+          {
+
+            var la=position.coords.latitude, lo=position.coords.longitude;
+            $.ajax({
+                      dataType: "json",
+                      url: "http://nominatim.openstreetmap.org/reverse",
+                      type: "get",
+                      data: {format: "json", lat:la, lon:lo}
+                    }).done(function(data) {
+                   	  document.getElementById("aviso").style.display="none";
+                   	  document.getElementById("perfil").style.display="flex";
+                      console.log(data);
+                      postcode= data.address.postcode;
+                      ciudad = data.address.town;
+                      document.getElementById("cityName").innerHTML=ciudad;
+                    });
+          }
+        );}
+      }
+
+}
+	  
