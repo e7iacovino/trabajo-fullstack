fae54092a5ba49a8ec614d2f10e98b0c4bcac8e6
Emiliano Iacovino <eiacovino@escuelasproa.edu.ar>
2023-06-23 10:51:53 -0300

    23/06 33.0

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ function after (ms, fn)
   setTimeout(fn, ms); 
 }
 
-var doc = new Document();
-doc.URL="...\index.html"
-doc.getElementById("p1").style.animation="appear 1s";
-after(1000, function(){doc.getElementById("p2").style.animation="appear 1s"});
-after(2000, function(){doc.getElementById("p3").style.animation="appear 1s"});
+
+document.getElementById("p1").style.animation="appear 3s";
+document.getElementById("p1").style.opacity="100%";
+after(200, function(){document.getElementById("p2").style.animation="appear 2s";document.getElementById("p2").style.opacity="100%";});
+after(400, function(){document.getElementById("p3").style.animation="appear 1s";document.getElementById("p3").style.opacity="100%";});
