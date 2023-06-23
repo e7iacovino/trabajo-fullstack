265339f2fe1cb32c195127cdead7b44187043a8c
Emiliano Iacovino <eiacovino@escuelasproa.edu.ar>
2023-06-23 10:24:38 -0300

    23/06

--- js/index.js
+++ js/index.js
@@ -1,33 +1,10 @@
-function auth_loc()
-{
-	var postcode, ciudad;
-      var script = document.createElement('script');
-      script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
-      document.getElementsByTagName('head')[0].appendChild(script);
-      script.onload=function()
-      {
-        if (navigator.geolocation){ 
-
-          navigator.geolocation.getCurrentPosition(function(position)
-          {
-
-            var la=position.coords.latitude, lo=position.coords.longitude;
-            $.ajax({
-                      dataType: "json",
-                      url: "http://nominatim.openstreetmap.org/reverse",
-                      type: "get",
-                      data: {format: "json", lat:la, lon:lo}
-                    }).done(function(data) {
-                   	  document.getElementById("aviso").style.display="none";
-                   	  document.getElementById("perfil").style.display="flex";
-                      console.log(data);
-                      postcode= data.address.postcode;
-                      ciudad = data.address.town;
-                      document.getElementById("cityName").innerHTML=ciudad;
-                    });
-          }
-        );}
-      }
-
+function after (ms, fn)
+{ 
+  setTimeout(fn, ms); 
 }
-	  
+
+var doc = new Document();
+doc.URL="...\index.html"
+doc.getElementById("p1").style.animation="appear 1s";
+after(1000, function(){doc.getElementById("p2").style.animation="appear 1s"});
+after(2000, function(){doc.getElementById("p3").style.animation="appear 1s"});
